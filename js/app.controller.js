ecfd15f6b45471e44cf111fe67c93d18e505f302
Malachi Partlow <malachipartlow@gmail.com>
2017-01-13 12:58:46 -0500

    mobile changes

--- js/app.controller.js
+++ js/app.controller.js
@@ -5,12 +5,18 @@
         .module('logger')
         .controller('AppController', Controller);
 
-    Controller.$inject = ['$scope', '$firebaseArray', '$timeout', '$http'];
+    Controller.$inject = ['$scope', '$firebaseArray', '$timeout', '$http', '$mdMedia'];
 
     /* @ngInject */
-    function Controller($scope, $firebaseArray, $timeout, $http) {
+    function Controller($scope, $firebaseArray, $timeout, $http, $mdMedia) {
       var self = this;
       $scope.service = {};
+      $scope.complete = false;
+      $scope.$watch(function() { return $mdMedia('max-width: 960px'); }, function(small) {
+        console.log(small);
+        $scope.screenIsSmall = small;
+      });
+      
       var currentRef = firebase.database().ref('track').on("value", function(values){
         $timeout(function(){
           $scope.current = values.val() ? values.val().currentAmount : 0;
@@ -24,29 +30,36 @@
 
 
       this.addHours = function(name, body, newAmount) {
-        console.log(name, body, newAmount);
         if(name && body && newAmount > 0){
           var hoursRef = firebase.database().ref('track').child('currentAmount');
-            hoursRef.transaction(function(hours) {
-              if(hours >= 0) {
-                 hours += newAmount;
-                 self.saveUser(name, body, newAmount);
-              }
-              else hours = newAmount;
-              return hours;
-            });    
+          hoursRef.transaction(function(hours) {
+            if(hours >= 0) {
+                hours += newAmount;
+                self.saveUser(name, body, newAmount);
+            }
+            else hours = newAmount;
+            return hours;
+          });    
         }
       }
 
       this.saveUser = function(name, body, hours) {
 
         $http.get('http://sau-geoiplookup.herokuapp.com/json/').then(function(response){
-          var data = response.data;
-          var location = data.city + data.region_name;
-          var cleanLoc = location.replace(/[.-]/g, "");
-          console.log(location, cleanLoc);
-          var userRef = firebase.database().ref().child('users/' + cleanLoc);
-          var locHoursRef = firebase.database().ref('users/' + cleanLoc).child('totalHours');       
+          var data = response.data,
+              location = data.city + data.region_name,
+              cleanLoc = location.replace(/[.-]/g, ""),
+              userRef = firebase.database().ref().child('users/' + cleanLoc),
+              locHoursRef = firebase.database().ref('users/' + cleanLoc).child('totalHours'),
+              infowindow = new google.maps.InfoWindow(),
+              center = new google.maps.LatLng(data.latitude + 2, data.longitude); 
+
+          userRef.update({
+            city: data.city,
+            state: data.region_name,
+            lat: data.latitude,
+            long: data.longitude,
+          });
           $firebaseArray(userRef).$add({
             hours: hours,
             reason: body,
@@ -54,34 +67,34 @@
             ip: data.ip,
             gaID: Cookies._ga
           });
-          userRef.update({
-            city: data.city,
-            state: data.region_name,
-            lat: data.latitude,
-            long: data.longitude,
-          });
-          locHoursRef.transaction(function(time) {
+          locHoursRef.transaction(function(time){
             if(time >= 0) time += hours;
             else time = hours;
+            $scope.complete = true;
             return time;
           });   
-        });
+
+          firebase.database().ref('users/' + cleanLoc).child('totalHours').on("value", function(values){
+            infowindow.setContent(
+              values.val() + ' hours from ' + data.city + ', ' + data.region_name
+            ); 
+          });  
+          infowindow.setPosition(center);
+          infowindow.open($scope.map);
+          });
       }
 
       $scope.showDirections = function(event, data){
-        var infowindow = new google.maps.InfoWindow();
-        console.log(data);
-        var center = new google.maps.LatLng(data.lat + 2, data.long);
-        var loc = data.city + data.state;
-        var cleanLoc = loc.replace(/[.-]/g, "");
+        var infowindow = new google.maps.InfoWindow(),
+            center = new google.maps.LatLng(data.lat + 2, data.long),
+            loc = data.city + data.state,
+            cleanLoc = loc.replace(/[.-]/g, "");
 
         firebase.database().ref('users/' + cleanLoc).child('totalHours').on("value", function(values){
           infowindow.setContent(
             values.val() + ' hours from ' + data.city + ', ' + data.state
           ); 
         });  
-
-
 
         infowindow.setPosition(center);
         infowindow.open($scope.map);
